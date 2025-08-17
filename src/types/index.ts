6074aa17f3390bb1dd43c282b55f278ccd8a895a
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-08-17 09:47:29 +0600

    image upload, dynamic input, day picker, modal for creating division

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,5 @@
 import type { ComponentType } from "react";
 
-
 export type { ISendOtp, IVerifyOtp, ILogin } from "./auth.type";
 
 export interface IResponse<T> {
@@ -20,3 +19,27 @@ export interface ISidebarItem {
 }
 
 export type TRole = "SUPER_ADMIN" | "ADMIN" | "USER";
+
+type ZodIssue = {
+  code: string;
+  expected: string;
+  received: string;
+  path: string[];
+  message: string;
+};
+
+type ErrorSource = {
+  path: string;
+  message: string;
+};
+
+export interface IErrorResponse {
+  success: boolean;
+  message: string;
+  errorSources?: ErrorSource[];
+  err?: {
+    issues: ZodIssue[];
+    name: string;
+  };
+  stack?: string;
+}
